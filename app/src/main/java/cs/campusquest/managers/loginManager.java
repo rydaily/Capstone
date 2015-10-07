fc94fc2c3b7d62f2e0e04524d11d1e815ad6781d
sauronkitty <evilmaniac@gmail.com>
2015-10-07 18:19:59 -0500

    Added comments to loginManager

--- app/src/main/java/cs/campusquest/managers/loginManager.java
+++ app/src/main/java/cs/campusquest/managers/loginManager.java
@@ -1,7 +1,13 @@
 package cs.campusquest.managers;
 
 public class loginManager {
+    /**
+     * The user's login ID
+     */
     private String sUsername;
+    /**
+     * The user's password.
+     */
     private String sPassword;
 
     public loginManager(){
@@ -23,5 +29,14 @@ public class loginManager {
     public void setPassword(String _sPassword){
         this.sPassword = _sPassword;
         return;
+    }
+
+    /**
+     * Call the serverAPI library to verify whether or not the login credentials provided are
+     * identical to those found on the server.
+     * @return True if the password and username are found on the server. False otherwise
+     */
+    public boolean verifyPassword(){
+        return(false);
     }
 }
