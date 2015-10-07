fc2ebeff4c9e3ca79c6826a9c42c4ca28639b769
sauronkitty <evilmaniac@gmail.com>
2015-10-07 18:33:24 -0500

    Added more functions to quest class

--- app/src/main/java/cs/campusquest/dataStructures/quest/quest.java
+++ app/src/main/java/cs/campusquest/dataStructures/quest/quest.java
@@ -1,7 +1,57 @@
 package cs.campusquest.dataStructures.quest;
 
-/**
- * Created by vv on 10/7/2015.
- */
 public class quest {
+    private int     iQuestID;
+    private String  sQuestName;
+    private String  sQuestDescription;
+    private boolean bIsComplete;
+
+    public quest(){
+        this(-1, null, null, false);
+        return;
+    }
+
+    public quest(int _iQuestID, String _sQuestName, String _sQuestDescription, boolean _bIsComplete){
+        setQuestID(_iQuestID);
+        setQuestName(_sQuestName);
+        setsQuestDescription(_sQuestDescription);
+        setCompletionFlag(_bIsComplete);
+    }
+
+    public void setQuestID(int _iQuestID){
+        this.iQuestID = _iQuestID;
+        return;
+    }
+
+    public void setQuestName(String _sQuestName){
+        this.sQuestName = _sQuestName;
+        return;
+    }
+
+    public void setsQuestDescription(String _sQuestDescription){
+        this.sQuestDescription = _sQuestDescription;
+        return;
+    }
+
+    public void setCompletionFlag(boolean _bIsComplete){
+        this.bIsComplete = _bIsComplete;
+        return;
+    }
+
+    /**
+     * Flags a quest as complete when called
+     */
+    public void setComplete(){
+        this.bIsComplete = true;
+        return;
+    }
+
+    /**
+     * Checks the quest's boolean flag to determine whether this quest has been flagged as completed
+     * or not.
+     * @return True if the quest has been flagged as complete, and false otherwise.
+     */
+    public boolean isComplete(){
+        return(this.bIsComplete);
+    }
 }
--- app/src/main/java/cs/campusquest/managers/questManager.java
+++ app/src/main/java/cs/campusquest/managers/questManager.java
@@ -1,7 +1,25 @@
 package cs.campusquest.managers;
 
-/**
- * Created by vv on 10/7/2015.
- */
+import cs.campusquest.dataStructures.quest.questList;
+
 public class questManager {
+    /**
+     * Contains reference to a linked list of objects. Each object will contain data relevant
+     * to a single quest that has either been loaded from the device's local database, or the
+     * server.
+     */
+    private questList hQuestList;
+
+    public questManager(){
+        this.hQuestList = new questList();
+        return;
+    }
+
+    private void populateQuestList(){
+
+    }
+
+    private void addQuest(){
+        
+    }
 }
