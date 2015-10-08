90b44a63607e5c2acb65366dcc289a8532e02403
sftwr <notnotvictor@gmail.com>
2015-10-07 20:14:55 -0500

    AppState has questManager
    allows all fragments and activities access to the questManager

--- app/src/main/java/cs/campusquest/AppState.java
+++ app/src/main/java/cs/campusquest/AppState.java
@@ -1,6 +1,7 @@
 package cs.campusquest;
 import android.app.Application;
 import android.content.Context;
+import cs.campusquest.managers.*;
 /**
  * by extending Application I can store objects/variables in it
  * which can then be accessed by activities using:
@@ -12,9 +13,20 @@ import android.content.Context;
  * Appstate myState = (AppState) getApplicationContext(); //used for activities
  */
 public class AppState extends Application{
+
     private static Context mContext;
+    questManager hQuestData;
 
     public AppState(){
+        hQuestData = null;
         this.mContext = this;
+    }
+
+    public static Context getContext(){
+        return mContext;
+    }
+
+    public void setQuestData(questManager _hQuestData){
+        hQuestData = _hQuestData;
     }
 }
